a2f67f7a92d35b24d4a55833c3fe1634b771e47b
Dany Savard <dany.savard@gmail.com>
2023-07-30 17:32:09 -0400

    Fouth iteration setups an nginx load balancer and add a new endpoint 'info' in the front service.

--- front/app/main.py
+++ front/app/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import socket
 
 app = FastAPI()
 
-@app.get("/ping")
-async def ping():
-    return {"ping": "pong"}
+@app.get('/')
+def root():
+	return "success"
+
+@app.get('/info')
+def info(request: Request):
+
+	resp = {
+		'connecting_ip': request.headers['X-Real-IP'],
+		'proxy_ip': request.headers['X-Forwarded-For'],
+		'host': request.headers['Host'],
+		'containe_id': socket.gethostname(),
+		'user-agent': request.headers['User-Agent']
+	}
+
+	return resp
+
+@app.get('/health-check')
+def flask_health_check():
+	return "success"
